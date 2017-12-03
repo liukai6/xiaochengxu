dc48bfdf0d3f00a60f82f4caf901fc9ceddb8e93
liukai6 <liu163p@gmail.com>
2017-12-03 17:56:50 +0800

    页面按钮布局完成，下面是头像的获取

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ App({
   globalData: {
     
   },
+  //获取头像
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据  
+    // var logs = wx.getStorageSync('logs') || []  
+    // logs.unshift(Date.now())  
+    // wx.setStorageSync('logs', logs)  
+  },
+  getUserInfo: function (cb) {
+    var that = this;
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口  
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo;
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      });
+    }
+  },  
   
   onShow: function (ops) {
     /* 在获取群聊 ID 之前，切记要先调用 wx.login
